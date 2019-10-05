87bc98f86a2440d9e3babf28c36b255c6eb6c0cb
Kajetan Dutka <keymax7@gmail.com>
2019-10-05 14:03:47 -0700

    refactor ball animations and replace HexGrid with hex_grid plus HexGrid

--- Main.gd
+++ Main.gd
@@ -6,7 +6,8 @@ const Ball1 = preload("res://scenes/Ball1.tscn")
 const Ball = preload("res://scripts/Ball.gd")
 
 var HexCell = preload("res://HexCell.gd").new()
-var HexGrid = preload("res://HexGrid.gd").new()
+var HexGrid = preload("res://HexGrid.gd")
+var hex_grid = HexGrid.new()
 
 onready var highlight = get_node("Highlight")
 onready var area_coords = get_node("Highlight/AreaCoords")
@@ -14,13 +15,14 @@ onready var hex_coords = get_node("Highlight/HexCoords")
 
 var paused = true
 var tick_timer = Timer.new()
+var interpolation_t: float = 0
 
 func _ready():
-	HexGrid.hex_scale = Vector2(50, 50)
+	hex_grid.hex_scale = Vector2(50, 50)
 	for i in range(-20, 20):
 		for j in range(-20, 20):
 			var cell = preload("res://scenes/Cell.tscn").instance()
-			cell.position = HexGrid.get_hex_center(Vector2(i, j))
+			cell.position = hex_grid.get_hex_center(Vector2(i, j))
 			cell.scale = Vector2(0.33, 0.33)
 			$BackgroundCellHolder.add_child(cell)
 	assert(OK == tick_timer.connect("timeout", self, "_game_tick"))
@@ -32,27 +34,29 @@ func _ready():
 
 	var offset01 = HexCell.DIR_NE + HexCell.DIR_SE
 	var b0 = Ball1.instance()
-	b0.hex_direction = HexCell.DIR_NE
-	b0.hex_position = HexCell.round_coords(offset01 - HexCell.DIR_NE)
-	b0.position = HexGrid.get_hex_center(b0.hex_position)
+	b0.init(HexCell.round_coords(offset01 - HexCell.DIR_NE), HexCell.DIR_NE, hex_grid)
 	self.add_child(b0)
 
 	var b1 = Ball1.instance()
-	b1.hex_direction = HexCell.DIR_SW
-	b1.hex_position = HexCell.round_coords(offset01 - HexCell.DIR_SW)
-	b1.position = HexGrid.get_hex_center(b1.hex_position)
+	b1.init(HexCell.round_coords(offset01 - HexCell.DIR_SW), HexCell.DIR_SW, hex_grid)
 	self.add_child(b1)
 
 	var i = 0
 	while i < len(HexCell.DIR_ALL):
 		var dir = HexCell.DIR_ALL[i]
 		var ball = Ball1.instance()
-		ball.hex_direction = dir
-		ball.hex_position = HexCell.round_coords(-(3 + i) * dir)
-		ball.position = HexGrid.get_hex_center(ball.hex_position)
+		ball.init(HexCell.round_coords(-(3 + i) * dir), dir, hex_grid)
 		self.add_child(ball)
 		i += 1
 	tick_timer.start()
+
+
+func _process(delta: float) -> void:
+	interpolation_t = min(1.0, interpolation_t + 2 * delta * (1.0 / Globals.TICK_TIME))
+	for child in self.get_children():
+		if child is Ball:
+			child.animation_process(interpolation_t)
+
 
 func _unhandled_input(event):
 	if 'position' in event:
@@ -61,11 +65,11 @@ func _unhandled_input(event):
 		if area_coords != null:
 			area_coords.text = str(relative_pos)
 		if hex_coords != null:
-			hex_coords.text = str(HexGrid.get_hex_at(relative_pos).axial_coords)
+			hex_coords.text = str(hex_grid.get_hex_at(relative_pos).axial_coords)
 
 		# Snap the highlight to the nearest grid cell
 		if highlight != null:
-			highlight.position = HexGrid.get_hex_center(HexGrid.get_hex_at(relative_pos))
+			highlight.position = hex_grid.get_hex_center(hex_grid.get_hex_at(relative_pos))
 
 	if event is InputEventKey:
 		if event.pressed and event.scancode == KEY_SPACE:
@@ -104,8 +108,7 @@ func _game_tick():
 
 	# apply movement
 	for ball in balls:
-		ball.hex_position = ball.target_hex_pos
-		ball.move_to(HexGrid.get_hex_center(ball.hex_position))  # todo: do this inside the ball
+		ball.move_to(ball.target_hex_pos)
 
 	# detect cell collisions (2+ balls entering the same cell)
 	for hex_pos in balls_moving_to:
@@ -115,4 +118,5 @@ func _game_tick():
 			continue
 		print("%d balls in cell %s" % [n, hex_pos])
 
+	interpolation_t = 0.0
 	print("Tick")
--- scripts/Ball.gd
+++ scripts/Ball.gd
@@ -1,37 +1,38 @@
 extends Node2D
 
 const Globals = preload("res://scripts/Globals.gd")
+const HexGrid = preload("res://HexGrid.gd")
 
+var hex_grid: HexGrid  # parent, set in _init
 var hex_position = Vector3.ZERO
 var hex_direction = Vector3.ZERO
-var target_hex_pos = Vector3.ZERO
-var target_screen_pos: Vector2
+var target_hex_pos = Vector3.ZERO  # scratchpad for Main, equal to hex_position outside of _game_tick
 var tier = 1
 
-var is_interpolating = false
-var interpolation_t = 0.0
-var interpolation_from: Vector2
-var interpolation_to: Vector2
+var interpolation_pos_from: Vector2
+var interpolation_pos_to: Vector2
 const CI_A = Vector2(0.5, 0)
 const CI_B = Vector2(0, 1)
 
-func _ready():
-	pass
 
-func _process(delta):
-	if is_interpolating:
+# WARNING: this is not _init (Script.new) but a custom init to be called after Node.instance
+func init(hex_pos: Vector3, hex_dir: Vector3, grid: HexGrid) -> void:
+	hex_grid = grid
+	hex_position = hex_pos
+	hex_direction = hex_dir
+	move_to(hex_position)
+
+
+func animation_process(progress: float):  # progress is between 0 and 1
+	if progress >= 1.0:
+		self.position = interpolation_pos_to
+	else:
 		# TODO: cubic interpolation destabilizes over time, find out why
-		# self.position = interpolation_from.cubic_interpolate(interpolation_to, CI_A, CI_B, interpolation_t)
-		self.position = interpolation_from.linear_interpolate(interpolation_to, interpolation_t)
-		interpolation_t += delta / Globals.TICK_TIME * 2 # speedup
-		if interpolation_t >= 1.0:
-			self.position = target_screen_pos # snap to the target position to avoid rounding errors
-			interpolation_t = 1.0
-			is_interpolating = false
+		# self.position = interpolation_pos_from.cubic_interpolate(interpolation_pos_to, CI_A, CI_B, progress)
+		self.position = interpolation_pos_from.linear_interpolate(interpolation_pos_to, progress)
 
-func move_to(new_screen_pos: Vector2):
-	target_screen_pos = new_screen_pos
-	interpolation_from = self.position
-	interpolation_to = target_screen_pos
-	interpolation_t = 0.0
-	is_interpolating = true
+
+func move_to(new_hex_pos: Vector3):
+	interpolation_pos_from = hex_grid.get_hex_center(hex_position)
+	interpolation_pos_to = hex_grid.get_hex_center(new_hex_pos)
+	hex_position = new_hex_pos
